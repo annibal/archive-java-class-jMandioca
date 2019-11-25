543155c06a545efaba82f020989987bf3c14d972
Arthur Annibal <arthur.annibal@gmail.com>
2019-11-25 00:13:33 -0300

    removed test files

--- src/mandioquito/Test_Random.java
+++
@@ -1,9 +0,0 @@
-package mandioquito;
-import java.util.Random;
-
-public class Test_Random {
-	public Test_Random() {
-		Random random = new Random();
-		System.out.println( Math.abs(random.nextInt())+"-"+Math.abs(random.nextInt()) );
-	}
-}
